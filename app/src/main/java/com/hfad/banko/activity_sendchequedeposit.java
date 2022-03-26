c643dff7e484ec7df438e41cc20df5f816d38cba
Camille <camille.downie.2021@uni.strath.ac.uk>
2022-03-26 15:55:21 +0000

    Updates:
    Back button on deposit completed is removed and the timer is set to 1500.
    Activities cheque deposit complete and send cheque deposit added with back buttons linked to scan back instead of payee and add payee pages.

---
+++ app/src/main/java/com/hfad/banko/activity_sendchequedeposit.java
@@ -0,0 +1,39 @@
+package com.hfad.banko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class activity_sendchequedeposit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sendchequedeposit);
+
+        Button deposit;
+        TextView back;
+
+            back = findViewById(R.id.txt_back);
+            back.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(activity_sendchequedeposit.this,activity_scanback.class);
+                    startActivity(intent);
+                }
+            });
+            deposit = findViewById(R.id.btn_add_payment);
+            deposit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(activity_sendchequedeposit.this , activity_chequedepositcomplete.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
+}
